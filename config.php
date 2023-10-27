66619f7948578b503cc12ac6032d695529e978f3
heich-hert <obinyherbert@gmail.com>
2023-10-27 16:58:01 +0300

    Sprint commit
    Sprint commit

---
+++ config.php
@@ -0,0 +1,33 @@
+
+<?php
+
+
+
+?>
+
+
+
+
+
+
+
+
+
+
+<!-- php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "phpMyAdimn";
+
+$conn = new mysqli($servername , $username, $password, $database);
+
+if ($conn->connect_error){
+    die(" Connection failed: " . $conn->connect_error);
+} else {
+    echo " Connected successfully";
+}
+
+$conn->close(); -->
+
+
